c215beb5a0ef5d7b013146da3e5a05108ed5955a
Der3c <Der3k@protonmail.com>
2017-05-03 16:12:21 -0500

    Generate initial JS grid

--- lab/etch-sketch/scripts/etch.js
+++ lab/etch-sketch/scripts/etch.js
@@ -1,3 +1,15 @@
+//This functions builds a grid inside of the "sketch" container
+function createGrid(x) {
+  for (var rows = 0; rows < x; rows++) {
+    for (var columns = 0; columns < x; columns++) {
+      $("#sketch").append("<div class='grid'></div>");
+    };
+  };
+  $(".grid").width(640/x);
+  $(".grid").height(640/x);
+};
+
 $(document).ready(function() {
+  createGrid(16);
 
 });
